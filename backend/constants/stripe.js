778197bcaf93b05b199896e2ac7be7ac0936fa6e
nerjib <kabirnajib0@gmail.com>
2022-02-06 17:07:03 +0100

    Updates

--- backend/constants/stripe.js
+++
@@ -1,9 +0,0 @@
-const configureStripe = require('stripe');
-
-const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-    ? 'sk_live_MY_SECRET_KEY'
-    : 'sk_test_51KPk4hG3qtUfMBk1g045CSDKbRInzr7aAC8pKMPzXUZzq1LJgADNGlmg1t5Odty74vcCVXScVopX5t2WAJLZfzYk00j0207GVb';
-
-const stripe = configureStripe(STRIPE_SECRET_KEY);
-
-module.exports = stripe;
--- src/constants/stripe.js
+++
@@ -1,5 +0,0 @@
-const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
-  ? 'pk_live_MY_PUBLISHABLE_KEY'
-  : 'pk_test_51KPk4hG3qtUfMBk1c7KIO4k2StNjba0iIYEzpevqteHPzWbHJzm1icERUEacR0wHSSoltqiiFZWkkLHnJ6JE1ZnP00yHF9YdQu';
-
-export default STRIPE_PUBLISHABLE;
